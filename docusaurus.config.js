7feb1705aa75cace3c7f91b1b0a646bd885016d6
josh-wong <23216828+josh-wong@users.noreply.github.com>
2024-07-12 18:18:01 +0900

    Add function to hide sidebar navigation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -167,6 +167,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       // Replace with your project's social card
       image: 'img/scalardl-logo.png',
       navbar: {
