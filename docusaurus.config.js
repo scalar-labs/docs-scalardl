07f1ba6311498ab42ecdca94dd2aa1c9cc33f2b8
Josh Wong <joshua.wong@scalar-labs.com>
2024-08-19 16:37:57 +0900

    Add progressive web app (PWA) support (#405)
    * Add `@docusaurus/plugin-pwa` plugin
    
    * Add ScalarDL icon
    
    * Create manifest.json
    
    * Add `plugin-pwa` configuration
    
    * Rename file
    
    * Create favicon.svg
    
    * Update PWA configuration
    
    * Change `start_url` and `background_color`
    
    * Update dependencies
    
    * Make icon transparent
    
    * Revert "Make icon transparent"
    
    This reverts commit 03e97379bb4f72713d975245d252b2cd15f21119.
    
    * Change colors for PWA
    
    * Create scalardl-app-icon.png
    
    * Change image type
    
    * Make icons transparent
    
    * Revert "Create scalardl-app-icon.png"
    
    This reverts commit e24da7ba97c3a2f11c9378f886cf24904d91d0c4.
    
    * Revert "Change image type"
    
    This reverts commit d941d83c946a3bc4dac0a0e0147fb9c54e6e85d9.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -150,6 +150,67 @@ const config = {
       },
     ],
     require.resolve('docusaurus-plugin-image-zoom'),
+    [
+      '@docusaurus/plugin-pwa',
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          'appInstalled',
+          'standalone',
+          'queryString',
+          'mobile',
+          'always',
+        ],
+        pwaHead: [
+          {
+            tagName: 'link',
+            rel: 'icon',
+            href: '/img/favicon.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'manifest',
+            href: '/manifest.json', // your PWA manifest
+          },
+          {
+            tagName: 'meta',
+            name: 'theme-color',
+            content: '#2673BB',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes',
+          },
+          {
+            tagName: 'meta',
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: '#2673BB',
+          },
+          {
+            tagName: 'link',
+            rel: 'apple-touch-icon',
+            href: '/img/favicon.png',
+          },
+          {
+            tagName: 'link',
+            rel: 'mask-icon',
+            href: '/img/favicon.svg',
+            color: '#2673BB',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileImage',
+            content: '/img/favicon.png',
+          },
+          {
+            tagName: 'meta',
+            name: 'msapplication-TileColor',
+            content: '#2673BB',
+          },
+        ],
+      },
+    ],
     // [
     //   require.resolve("docusaurus-lunr-search"),
     //   {
