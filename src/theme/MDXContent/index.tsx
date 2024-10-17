896ebe05ca3de0b264a2629f66d96e138494399a
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2024-10-17 16:05:33 +0900

    Create index.tsx
    Swizzle component to add glossary tooltip function.

---
+++ src/theme/MDXContent/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MDXContent from '@theme-original/MDXContent';
+import type MDXContentType from '@theme/MDXContent';
+import type {WrapperProps} from '@docusaurus/types';
+
+type Props = WrapperProps<typeof MDXContentType>;
+
+export default function MDXContentWrapper(props: Props): JSX.Element {
+  return (
+    <>
+      <MDXContent {...props} />
+    </>
+  );
+}
