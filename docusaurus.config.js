a1ee4b1fa4447824ae92221c8bb7c7a75ee6f11b
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2025-03-25 17:53:24 +0900

    Make all broken links show warning
    A list of all broken links for versions of docs still under Maintenance Support are generated in **brokenLinks.log**. To eliminate potentially redundant entries in the log, only `onBrokenLinks` is set to `warn`.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,7 +24,8 @@ const config = {
   projectName: 'docs-scalardl', // Usually your repo name.
 
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'ignore',
+  onBrokenAnchors: 'ignore',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
