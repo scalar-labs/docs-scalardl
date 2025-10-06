7eeeb9524c6b48195ed9f8d7c7359aac9c27faa0
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2025-10-06 16:35:49 +0900

    Add `onDuplicateRoutes` configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,6 +25,7 @@ const config = {
 
   onBrokenLinks: 'warn',
   onBrokenAnchors: 'ignore',
+  onDuplicateRoutes: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
