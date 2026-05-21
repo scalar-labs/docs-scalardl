799110ad1a4859e53254a823064ce72e99793461
Josh Wong <23216828+josh-wong@users.noreply.github.com>
2026-05-21 17:29:42 +0900

    Remove announcement placeholder
    Since we always show an announcement, there's no need to have a placeholder for it.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -470,6 +470,7 @@ const config = {
         content:
           'Announcing the release of ScalarDL 3.13!🚀 For details on what\'s included in this new version, see the <a target="_self" href="/docs/latest/releases/release-notes?utm_source=docs-site&utm_medium=announcementbar">release notes</a>.',
           // '<b>Announcing the release of ScalarDL X.X!🚀 For details on what\'s included in this new version, see the <a target="_self" href="/docs/latest/releases/release-notes?utm_source=docs-site&utm_medium=announcementbar">release notes</a>.</b>',
+          'Announcing the release of ScalarDL 3.13!🚀 For details on what\'s included in this new version, see the <a target="_self" href="/docs/latest/releases/release-notes?utm_source=docs-site&utm_medium=announcementbar">release notes</a> and <a target="_self" href="XXX?utm_source=docs-site&utm_medium=announcementbar">blog post</a>.',
         backgroundColor: '#2673BB',
         textColor: '#FFFFFF',
         isCloseable: false,
